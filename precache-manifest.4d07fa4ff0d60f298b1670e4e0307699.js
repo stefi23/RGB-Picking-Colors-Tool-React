bd0a9a62eff9043a04d66354ac78e6af035769c9
stefi23 <stefania.rosca2309@gmail.com>
2019-11-21 17:52:22 +0100

    Updates

---
+++ precache-manifest.4d07fa4ff0d60f298b1670e4e0307699.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0406635b6d536e424a6454af1000ee80",
+    "url": "/Quote-Generator-With-React/index.html"
+  },
+  {
+    "revision": "7da604067fb6cb68e349",
+    "url": "/Quote-Generator-With-React/static/css/main.08d683a1.chunk.css"
+  },
+  {
+    "revision": "40c182dc5629982a13e0",
+    "url": "/Quote-Generator-With-React/static/js/2.d16060c7.chunk.js"
+  },
+  {
+    "revision": "7da604067fb6cb68e349",
+    "url": "/Quote-Generator-With-React/static/js/main.b188d12e.chunk.js"
+  },
+  {
+    "revision": "f4ceb1643b0bbec930f9",
+    "url": "/Quote-Generator-With-React/static/js/runtime-main.ab3d6d0b.js"
+  }
+]);
